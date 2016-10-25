96d106f5a4720399f06147024e35ae657c57d255
Jonatan Burgos Gonzalez <Jonatan Burgos Gonzalez>
2016-10-25 12:22:53 -0300

    Se modifico el index

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-	echo "Hola mundo"
-<
+<doctype>
+<html>
+	<head>
+		<body>
+			<h1>
+				<?php echo "Hola mundo" ?>
+			</h1>
+				<?php
+					for($i=2; $i<6; $i++)
+						echo "<h$i>Disminuyendo el tamaño del titulo</h$i>"
+				?>
+			
+		</body>
+	</head>
+
+
+</html>
